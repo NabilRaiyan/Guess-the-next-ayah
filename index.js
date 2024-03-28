8b0f7bfed9eee1f229eb24df61b7e9ec269b8633
NabilRaiyan <rraiyan77@gmail.com>
2024-03-28 15:50:41 +0600

    api call

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log(document.getElementById("title").innerHTML);
+
+
+async function getSurah(){
+    await fetch("http://api.alquran.cloud/v1/surah")
+        .then((response)=>response.json())
+        .then((surah)=>
+        {
+            console.log(surah.data[0]['englishName'])
+        }
+        )
+    
+    
+}
+
+
+
+getSurah();
+
+
