0db79b8d5a38c85b4333b0854fe777fa5a9267c8
NabilRaiyan <rraiyan77@gmail.com>
2024-05-26 22:00:14 +0600

    configuring tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="bg-red-200">
-      <h1 className="text-2xl text-slate-600">Hello World!</h1>
+      <h1 className="text-2xl text-slate-600">Hello World! My name is Raiyan</h1>
     </div>
   )
 }
