f8b2ec5aa3c71fa0ca940d3e093623056e42d35b
NabilRaiyan <rraiyan77@gmail.com>
2024-05-27 21:44:12 +0600

    fetching data from the api

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ const App = ()=> {
     .then((response) => response.json())
     .then((ayah) => console.log(ayah.data.text));
   
-  
   return (
     <div className="bg-red-200">
       <h1 className="text-2xl text-slate-600">Hello World! My name is Raiyan</h1>
